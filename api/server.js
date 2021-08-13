0b5e9c11bec97658cb3530ee5bbffafcaca142d5
Rodgers31 <otienor986@gmail.com>
2021-08-13 17:23:41 -0400

    set up server

--- api/server.js
+++ api/server.js
@@ -1,1 +1,8 @@
 // build your server here and require it from index.js
+
+const express = require('express');
+
+const server = express();
+server.use(express.json());
+
+module.exports = server;
--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // start your server here
+
+const server = require('./api/server');
+
+const port = process.env.port || 3000;
+
+server.listen(port, () => {
+	console.log(`Server is running on port ${port}`);
+});
